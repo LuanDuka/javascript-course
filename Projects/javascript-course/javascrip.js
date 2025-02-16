b6218d281bb28910c762bb80b3f66e0c5a7e5eb7
Luan Duka <luancs21@icloud.com>
2025-02-16 14:21:43 -0300

    Add comments on js code: add permantent phrase to player make a move: add a static results

--- Projects/javascript-course/javascrip.js
+++ Projects/javascript-course/javascrip.js
@@ -268,17 +268,21 @@ if (age >= 16) {
   console.log("You can't drive");
 }
 
-// Rock Paper Scissors
+// Rock Paper Scissors Lesson 06
 
 let winScore = 0;
 let lossScore = 0;
 let tieScore = 0;
 
+document.getElementById('result').innerHTML = "Choose a move!"
+
+document.getElementById('scorePoints').innerHTML = `Wins: ${winScore}.Losses ${lossScore}.Ties ${tieScore}`;
+
 function choice(playerMove) {
-  // Gera a jogada do computador
+  // Generate a random number
   const randomNumber = Math.random();
   let computerMove = '';
-
+  //Define what choice the computer makes
   if (randomNumber >= 0 && randomNumber < 1 / 3) {
     computerMove = 'Rock';
   } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
@@ -286,11 +290,10 @@ function choice(playerMove) {
   } else {
     computerMove = 'Paper';
   }
-
+  //Show the results
   let result = '';
 
-  // Lógica para determinar o resultado
-
+  //Algorithm to define the results
   if (playerMove === computerMove) {
     result = 'Tie.';
   } else if (
@@ -303,12 +306,10 @@ function choice(playerMove) {
     result = 'You lose!';
   }
 
-
   // Shows result in the page
   document.getElementById('result').innerHTML = `You picked: ${playerMove}.Computer picked ${computerMove}.${result}`;
 
   //Score result
-
   if (result === 'Tie.') {
     tieScore++;
   } else if (result === 'You win!') {
@@ -366,4 +367,4 @@ console.log(crrnc);
 
 //Exercises Lesson 6
 const hour = new Date();
-document.getElementById('exercise6a').innerHTML = hour;
+// document.getElementById('exercise6a').innerHTML = hour;
