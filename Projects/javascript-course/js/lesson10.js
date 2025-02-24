aff3707eb84b432f15e178935ce086fc6119726f
Luan Silva <ipq.qualidade@gmail.com>
2025-02-24 17:46:26 -0300

    Update toggled button

--- Projects/javascript-course/js/lesson10.js
+++ Projects/javascript-course/js/lesson10.js
@@ -2,14 +2,14 @@
 const buttonTest = document.querySelector('.js-test-button');
 console.log(buttonTest.classList);
 
-function toggledOnOff() {
-    const buttonElement = document.querySelector('.js-toggled-button');
-    console.log(buttonElement.classList);
+function toggledOnOff(selector) {
+    const button = document.querySelector(selector);
+    console.log(button.classList);
 
-    if (!buttonElement.classList.contains('is-toggled')) {
-        buttonElement.classList.add('is-toggled');
+    if (!button.classList.contains('is-toggled')) {
+        button.classList.add('is-toggled');
 
     } else {
-        buttonElement.classList.remove('is-toggled');
+        button.classList.remove('is-toggled');
     }
 }
