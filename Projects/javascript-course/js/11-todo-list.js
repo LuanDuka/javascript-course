cb06aae2ce132373c6aacf64d729ec826ca482ba
Luan Silva <ipq.qualidade@gmail.com>
2025-02-26 13:29:39 -0300

    Add lesson 11 exercises a-l

--- Projects/javascript-course/js/11-todo-list.js
+++ Projects/javascript-course/js/11-todo-list.js
@@ -83,7 +83,7 @@ for (let i = 0; i <= todoListSecond.length - 1; i++) {
 //Accumulator Pattern
 //1.Create a variable to store the result
 //2.Loop through the array and update the result
-const nums = [1, 1, 3];
+const nums = [1, 1, 3];//sum all the elements
 let total = 0;//Accumulator Variable > result is a number
 
 for (let i = 0; i < nums.length; i++) {
@@ -146,3 +146,102 @@ function addTodo() {
 //1.Save the data       >> const todoList = ['make dinner', 'wash dishes'];
 //2.Generate the HTML   >> let todoListHTML = '';...for (let i ...const html...
 //3.Make it interactive
+
+//Lesson 11 Exercises
+//11a
+const numbs = [10, 20, 30];
+numbs[2] = 45;
+console.log(numbs);
+//11b
+function getLastValue(array) {
+    const lastIndex = array.length - 1;
+    return array[lastIndex];
+}
+// console.log(getLastValue([1, 20, 22, 24, 5]));
+// console.log(getLastValue(['hi', 'hello', 'hey']));
+//11c
+function arraySwap(array) {
+    const finalIndex = array.length - 1;
+    // We need to save the values first before swapping.
+    // Otherwise, if we swap directly,
+    // we would lose one of the values.
+    const lastValue = array[finalIndex];
+    const firstIndex = array[0];
+
+    array[0] = lastValue;
+    array[finalIndex] = firstIndex;
+
+    return array;
+}
+
+console.log(arraySwap([1, 20, 22, 24, 5]));
+console.log(arraySwap(['hi', 'hello', 'hey']));
+//11d
+for (i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+//11e
+for (i = 5; i >= 0; i--) {
+    console.log(i);
+
+}
+//11f
+let id11 = 0;
+while (id11 <= 10) {
+    console.log(id11);
+    id11 += 2;
+}
+let ie11 = 5;
+
+while (ie11 > 0) {
+    console.log(ie11);
+    ie11--;
+}
+//11h
+function addOne(array) {
+    const increased = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const byOne = array[i];
+        increased.push(byOne + 1);
+    }
+    return increased
+}
+console.log(addOne([1, 2, 3]));
+console.log(addOne([-2, -1, 0, 99]));
+//11l
+function minMax(nums) {
+    let min = null;// Set the starting to null
+    let max = null;
+    //let min = null;// Initialize with the first element
+    //let max = null;
+
+    for (let i = 0; i < nums.length; i++) {
+        const current = nums[i];
+
+        if (min === null || current < min) {
+            min = current;// Update min if current element is smaller or null
+        }
+        if (max === null || current > max) {
+            max = current;// Update max if current element is larger or null
+        }
+    }
+
+    return { min, max }// Return an object with min and max
+}
+console.log(minMax([1, -3, 5]));
+console.log(minMax([-2, 3, -5, 7, - 10]));
+console.log(minMax([]));
+console.log(minMax([3]));
+//11n
+function countWords(words) {
+    let property = '';
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        property = object[word];
+        console.log(property);
+    }
+    return word
+}
+console.log(countWords(['apple', 'grape', 'apple', 'apple']));
