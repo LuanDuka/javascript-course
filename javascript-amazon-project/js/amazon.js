55bc93a607f4c63de8057f7fd1f26a0960047d8d
Luan Silva <ipq.qualidade@gmail.com>
2025-03-05 14:22:03 -0300

    use products.js files

--- javascript-amazon-project/js/amazon.js
+++ javascript-amazon-project/js/amazon.js
@@ -6,31 +6,6 @@ Main idea of JavaScrip
 */
 console.log('hello');
 //data structure: combination of objects and arrays  
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090//Best practice calculate in cents
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095//Best practice calculate in cents
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799//Best practice calculate in cents
-}];
 
 let productsHTML = '';
 
