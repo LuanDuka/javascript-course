af49ee5631e4942e1f3014856366af73ae700008
Luan Silva <ipq.qualidade@gmail.com>
2025-03-06 14:50:23 -0300

    Generate HTML for the order summary

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,8 +1,17 @@
 
 //Get a Variable Out of a Files
 //1. Add type="module"attribute
+
+import { products } from '../data/products.js';
+
 //2. Export
-export const cart = [];
+export const cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+}, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+}];
 //3. Import
 
 export function addToCart(productId, quantity) {
