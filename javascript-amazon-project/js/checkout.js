a43ac7aef74ccb06ae6e17113205b4aa317481e1
Luan Duka <luancs21@icloud.com>
2025-03-09 08:05:04 -0300

    Add eventListeners to "Save" links and "Update" link desappear when editing

--- javascript-amazon-project/js/checkout.js
+++ javascript-amazon-project/js/checkout.js
@@ -42,7 +42,10 @@ cart.forEach((cartItem) => {
                  Update
                 </span>
                 <input class="quantity-input">
-                <span class="save-quantity-link link-primary">Save</span>
+                <span class="save-quantity-link link-primary js-save-link"
+                data-product-id="${matchingProduct.id}">
+                Save
+                </span>
                 <span class="delete-quantity-link link-primary js-delete-link" 
                   data-product-id="${matchingProduct.id}">
                   Delete
@@ -133,3 +136,15 @@ document.querySelectorAll('.js-update-link')
       container.classList.add('is-editing-quantity');
     });
   });
+
+document.querySelectorAll('.js-save-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const { productId } = link.dataset;
+
+      const container = document.querySelector(
+        `.js-cart-item-container${productId}`
+      );
+      container.classList.remove('is-editing-quantity');
+    })
+  })
