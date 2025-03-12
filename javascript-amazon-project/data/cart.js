c465c749388ce5f907c0284ccb4c36c0f8efb7c0
Luan Silva <ipq.qualidade@gmail.com>
2025-03-12 14:52:19 -0300

    Create automated test for addToCart

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -2,19 +2,26 @@
 //Get a Variable Out of a Files
 //1. Add type="module"attribute
 //2. Export
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if (!cart) {
-    cart = [{
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2,
-        deliveryOptionId: '1'
-    }, {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1,
-        deliveryOptionId: '2'
-    }];
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = [{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2,
+            deliveryOptionId: '1'
+        }, {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1,
+            deliveryOptionId: '2'
+        }];
+    }
 }
+
 //3. Import
 
 function saveToStorage() {
