4d6c0885125044abcb23a8dee3612d054b5589fd
Luan Silva <ipq.qualidade@gmail.com>
2025-03-26 15:54:20 -0300

    Add formulário de inpeção files and some exercises of algorism

---
+++ programming-logic/js/script.js
@@ -0,0 +1,94 @@
+//Exemplo 2.1
+// cria referência ao form e ao elemento h3 (onde será exibida a resposta)
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3"); // primeiro elemento h3 da página
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+  const nome = frm.inNome.value; // obtém o nome digitado no form
+  resp.innerText = `Olá ${nome}!`; // exibe a resposta do programa
+  e.preventDefault()  // evita envio do form
+})
+
+
+const cor = document.querySelector("#inCor")    // elemento com id=inCor
+const lista = document.querySelector(".lista")  // elemento da class=lista
+
+
+/*
+const nome = prompt("Qual é seu nome?");
+alert(`Olá ${nome}`);
+*/
+
+//1.9 1.3
+/*
+const a = "20"
+const b = a * 2    // b = 40
+const c = a / 2    // c = 10
+const d = a - 2    // d = 18
+const e = Number(a) + 2    // e = 202 ???
+console.log("e: " + e)   // exibe o valor de uma variável
+
+const fruta = "Banana"
+const preco = 3.50
+const levar = true
+let novoValor
+console.log(typeof fruta)      // string
+console.log(typeof preco)      // number
+console.log(typeof levar)      // boolean
+console.log(typeof novoValor)  // undefined
+//inteiro ou decimal
+console.log(Number.isInteger(12))    // true
+console.log(Number.isInteger(3.50))  // false
+*/
+//1.10
+/*
+const numero = Number(prompt("Digite um número"));// lê dado de entrada e converte para número
+const dobro = numero + numero;
+console.log(`O dobro é ${dobro}`)
+*/
+//1.6
+/*
+const number1 = Number(prompt("First number:"));
+const number2 = Number(prompt("Second number:"));
+const plusNumbers = number1 + number2;
+console.log(`The result is: ${plusNumbers}`);
+*/
+//1.7
+/*
+const dinnerValue = Number(prompt("Dinner cost:"));
+const taxDinner = dinnerValue * 0.10;
+const totalDinner = dinnerValue + taxDinner;
+console.log(
+  `Waiter tax $${taxDinner.toFixed(2)}\nTotal $${totalDinner.toFixed(2)}`
+);
+*/
+//1.7 d
+/*
+const travelDays = Number(prompt("Dias de viagem:"));
+const travelHours = Number(prompt("Horas de viagem"));
+const totalHours = (travelDays * 24) + travelHours;
+console.log(`Sua viagem vai durar ${totalHours}`);
+*/
+//vizinhos a
+/*
+const neiborNumber = Number(prompt("Número:"));
+const anterior = neiborNumber - 1;
+const posterior = neiborNumber + 1;
+console.log(`São vizinhos ${anterior} e ${posterior}`);
+*/
+//clientes pagar b
+/*
+const totalPizza = Number(prompt("Pizza: $"));
+const clientes = Number(prompt("Clientes:"));
+const valorCliente = totalPizza / clientes;
+console.log(`Valor por cliente $${valorCliente.toFixed(2)}`);
+*/
+//à vista ou parcelado
+/*
+const valor = Number(prompt("Valor produto:"));
+const desconto = valor * 0.10;
+const aVista = valor - desconto;
+const parcelado = valor / 3;
+console.log(`Preço $${valor}\nÀ vist $${aVista}\nOu 3x de $${parcelado.toFixed(2)}`);
+*/
+//
