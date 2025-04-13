79ccd3253afd35e33f4aab915b8a11802a769c5b
Luan Duka <luancs21@icloud.com>
2025-04-13 10:59:40 -0300

    Debug comma in the last number, change to dot

--- livrojs/cap05/js/descending.js
+++ livrojs/cap05/js/descending.js
@@ -5,10 +5,10 @@ frm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const number = Number(frm.inNumber.value);
-  let resposta = `Entre ${number} e 1: `;
+  let resposta = `Entre ${number} e 1: ${number}, `;
 
-  for (let i = number; i > 0; i--) {
+  for (let i = number - 1; i > 1; i--) {
     resposta = resposta + i + " , ";
   }
-  resp.innerText = resposta;
+  resp.innerText = resposta + "1.";
 })
