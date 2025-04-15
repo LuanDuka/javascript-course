9a45f195afb1bbcfe50998157647ff0fa6ddacb2
Luan Silva <ipq.qualidade@gmail.com>
2025-04-15 13:00:18 -0300

    Add optimization on for loop, with num/2

--- livrojs/cap05/js/prime-numbers.js
+++ livrojs/cap05/js/prime-numbers.js
@@ -6,17 +6,33 @@ frm.addEventListener("submit", (e) => {
 
   const num = Number(frm.inNumber.value);
   let numDivisers = 0;
+  let haveDivisor = 0;
+  /*
+    for (let i = 1; i <= num; i++) {
+      if (num % i == 0) {
+        numDivisers++;
+      }
+    }
+    if (numDivisers == 2) {
+      resp.innerText = `${num} Is prime`;
+    } else {
+      resp.innerText = `${num} Is not prime`
+    }
+  */
 
-  for (let i = 1; i <= num; i++) {
+  for (let i = 2; i <= num / 2; i++) {
     if (num % i == 0) {
-      numDivisers++;
+      haveDivisor = 1;
+      break;
     }
   }
-  if (numDivisers == 2) {
+
+  if (num > 1 && !haveDivisor) {
     resp.innerText = `${num} Is prime`;
   } else {
     resp.innerText = `${num} Is not prime`
   }
+
 
   frm.inNumber.value = "";
   frm.inNumber.focus();
