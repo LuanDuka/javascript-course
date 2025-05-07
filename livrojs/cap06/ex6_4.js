397dd0a97fa1693fe1970f69e7e76a7e3fed6baa
Luan Silva <ipq.qualidade@gmail.com>
2025-05-07 13:23:12 -0300

    Add some comments

--- livrojs/cap06/ex6_4.js
+++ livrojs/cap06/ex6_4.js
@@ -1,60 +1,62 @@
 const frm = document.querySelector("form");
 const resp = document.querySelector("pre");
 
-const children = [];
+const children = []; //declare global array
 
 frm.addEventListener("submit", (e) => {
-  e.preventDefault();
+  e.preventDefault(); //prevent send the form
 
-  const name = frm.inName.value;
+  const name = frm.inName.value; //get field content
   const age = Number(frm.inAge.value);
 
-  children.push({ name, age });
+  children.push({ name, age }); //add data to the array
   frm.reset(); //clean form fields
-  frm.inName.focus();
-  frm.btList.dispatchEvent(new Event("click"));
+  frm.inName.focus(); //go to form field
+  frm.btList.dispatchEvent(new Event("click")); //make the click event
 })
 
 frm.btList.addEventListener("click", () => {
-  if (children.length == 0) {
+  if (children.length == 0) { //if empity shows an alert
     alert("There're no children on the list");
     return;
   }
 
-  let list = "";
+  let list = ""; //to concatenate children list
   for (const child of children) {
-    const { name, age } = child;
+    const { name, age } = child; //destructure the object
     list += name + " - " + age + " years\n";
   }
-  resp.innerText = list;
+  resp.innerText = list; // show the list
 })
 
 frm.btSummarize.addEventListener("click", () => {
   if (children == 0) {
     alert("There're no children on the list");
   }
-  const copy = [...children];
-  copy.sort((a, b) => a.age - b.age);
+  const copy = [...children]; //create a copy of children array
+  copy.sort((a, b) => a.age - b.age); //order by age
 
   let resume = ""; //to concatenate output
-  let aux = copy[0].age;
-  let names = [];
+  let aux = copy[0].age; //lowest age from the array
+  let names = []; //to insert names of each age
 
-  for (const child of copy) {
+  for (const child of copy) { //loop through children copy array
     const { name, age } = child;
 
-    if (age == aux) {
-      names.push(name)
-    } else {
+    if (age == aux) { //if same age
+      names.push(name) //add to names array
+
+    } else { //if is not, makes a summarize to each age
       resume += aux + " year(s):" + names.length + "child(ren) - "
       resume += ((names.length / copy.length) * 100).toFixed(2) + "%\n"
       resume += "(" + names.join(", ") + ")\n\n"
 
-      aux = age;
-      names = [];
-      names.push(name)
+      aux = age; //gets the new age in order
+      names = []; //clean array names
+      names.push(name) //adds the first of the new age
     }
   }
+  //add last child
   resume += aux + " year(s):" + names.length + "child(ren) - "
   resume += ((names.length / copy.length) * 100).toFixed(2) + "%\n"
   resume += "(" + names.join(", ") + ")\n\n"
