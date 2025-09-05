f3493e6f081e12be096add61ee8be7a234f8a4d3
Luan Silva <ipq.qualidade@gmail.com>
2025-09-05 11:08:37 -0300

    test some C codes

--- CS50/Week 1/C/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-  int number = 42;
-  printf("hello, luan\n");
-  printf("The number is: %d\n", number);
-  return 0;
-}
---
+++ CS50/Week1/C/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int number = 42;
+  printf("hello, world\n");
+  printf("The answer of the universe is: %d\n", number);
+  return 0;
+}
