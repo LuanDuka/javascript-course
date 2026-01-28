a4dab262f9ab0108d2e579ef516d63843d17c142
Luan Silva <ipq.qualidade@gmail.com>
2026-01-28 13:09:34 -0300

    Lesson Students Grades

---
+++ livrojs/cap06/ex6_10.js
@@ -0,0 +1,23 @@
+const prompt=require("prompt-sync")()
+console.log("Inform the students. Then, type 'End' next to their name to exit")
+const students=[] // declare vector
+do{
+  const name = prompt("Name: ") // read name
+  if(name=="End"){ // before reading the grade, check
+    break // exit the repeat
+  }
+  const grade=Number(prompt("Grade:")) // read grade
+  students.push({name,grade}) // add date to the object vector
+  console.log("Ok! Student registered...") 
+}while(true)
+  console.log("-".repeat(40)) // display 40
+  const higher = students.reduce((a,b)=> Math.max(a,b.grade),0) // gets the highest note
+  console.log(`Highest grade: ${higher}`) 
+  if(higher >=7){
+    const highlights=students.filter(student => student.grade == higher)
+    for(const highlight of highlights){
+console.log(`-${highlight.name}`)
+    }
+  }else{
+    console.log("There are no hightlight students in the class")
+  }
